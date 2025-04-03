e7345ce966c79559d2c8b85f7aae3f5c97f2118f
Bryanprestige <bryanhvaldez@gmail.com>
2025-04-03 09:42:28 +0200

    feat: :art: first practice
    finished first practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import reactIcon from './assets/react-icon.png'
 import './App.css'
 
 function App() {
@@ -8,10 +9,22 @@ function App() {
   return (
     <main>
       <header>
-          <img src={reactLogo} className="logo react" alt="React logo" />    
+          <img src={reactLogo} className="logo-react" alt="React logo" />    
           <h1>ReactFacts</h1>
       </header>
-      <h1></h1>
+      <section className='facts'>
+        <h1 className='title'>Fun facts about React</h1>
+        <div className='ul-img'> 
+          <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+          <img src={reactIcon} className="logo-react-back" alt="React logo" />  
+        </div>
+      </section>
     </main>
   )
 }
