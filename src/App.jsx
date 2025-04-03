562135de63a1b792f292aaf0dc02e8b880b5c014
Bryanprestige <bryanhvaldez@gmail.com>
2025-04-03 09:20:08 +0200

    feat: :tada: react project
    started react scrimba project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <header>
+          <img src={reactLogo} className="logo react" alt="React logo" />    
+          <h1>ReactFacts</h1>
+      </header>
+      <h1></h1>
+    </main>
+  )
+}
+
+export default App
